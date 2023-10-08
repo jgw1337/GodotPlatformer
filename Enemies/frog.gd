6748ac6c831cccafd2c57f0cf1ae68ee3220290d
jgw <8540274+jgw1337@users.noreply.github.com>
2023-10-07 17:14:21 -0700

    add frog death, player health, & player dmg

--- Enemies/frog.gd
+++ Enemies/frog.gd
@@ -8,23 +8,28 @@ var is_chasing = false
 @onready var player = get_node("../../Player/MainCharacter")
 @onready var frog = get_node("AnimatedSprite2D")
 
+func _ready():
+	frog.play("Idle")
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
 
-	if is_chasing:
-		var direction = (player.position - self.position).normalized()
-		frog.play("Jump");
-	
-		if direction.x > 0:
-			frog.flip_h = true
+	if frog.animation != "Death":
+		if is_chasing:
+			var direction = (player.position - self.position).normalized()
+			frog.play("Jump");
+		
+			if direction.x > 0:
+				frog.flip_h = true
+			else:
+				frog.flip_h = false
+		
+			velocity.x = direction.x * SPEED
 		else:
-			frog.flip_h = false
-	
-		velocity.x = direction.x * SPEED
-	else:
-		frog.play("Idle");
-		velocity.x = 0
+			velocity.x = 0
+			frog.play("Idle")
 
 	move_and_slide()
 
@@ -36,3 +41,20 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "MainCharacter":
 		is_chasing = false
+
+
+func _on_frog_death_body_entered(body):
+	if body.name == "MainCharacter":
+		death()
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "MainCharacter":
+		body.health -= 3
+		death()
+	
+func death():
+	is_chasing = false
+	frog.play("Death");
+	await frog.animation_finished
+	self.queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var health = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -44,3 +44,7 @@ func _physics_process(delta):
 		anim.play("Fall")
 
 	move_and_slide()
+
+	if health <= 0:
+		queue_free()
+		get_tree().change_scene_to_file("res://main.tscn")
